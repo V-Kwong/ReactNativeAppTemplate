81a157935eb259bc89aec3e908769c8d8cd55fde
V-Kwong <vincent_kwong578@hotmail.com>
2023-10-23 15:33:22 -0700

    installed react native paper

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StatusBar, View} from 'react-native';
+import {DefaultTheme, PaperProvider} from 'react-native-paper';
 import {
   DefaultTheme as NavigatorDefaultTheme,
   NavigationContainer,
@@ -8,7 +9,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {navigationRef} from './RootNavigation';
 
-import getGlobalStyles from '../utils/styles';
+import getGlobalStyles, {Colors} from '../utils/styles';
 
 import RegisterScreen from '../screens/unauth/RegisterScreen';
 import SignInScreen from '../screens/unauth/SignInScreen';
@@ -29,19 +30,36 @@ export default function AppNavigator() {
   const darkMode = false;
   const globalStyles = getGlobalStyles(false);
 
+  const theme = {
+    ...DefaultTheme,
+    // roundness: 2,
+    dark: darkMode,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: Colors.primary_orange,
+      accent: Colors.primary_orange,
+      text: globalStyles.text.color,
+      placeholder: globalStyles.inactive_text.color,
+      error: globalStyles.error_text.color,
+      background: 'transparent',
+    },
+  };
+
   return (
-    <View style={globalStyles.container}>
-      <StatusBar
-        barStyle={darkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={globalStyles.container.backgroundColor}
-        translucent
-      />
-      <NavigationContainer ref={navigationRef} theme={navTheme}>
-        <ParentStack.Navigator screenOptions={SCREEN_OPTIONS}>
-          <ParentStack.Screen name="Register" component={RegisterScreen} />
-          <ParentStack.Screen name="SignIn" component={SignInScreen} />
-        </ParentStack.Navigator>
-      </NavigationContainer>
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={globalStyles.container}>
+        <StatusBar
+          barStyle={darkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={globalStyles.container.backgroundColor}
+          translucent
+        />
+        <NavigationContainer ref={navigationRef} theme={navTheme}>
+          <ParentStack.Navigator screenOptions={SCREEN_OPTIONS}>
+            <ParentStack.Screen name="Register" component={RegisterScreen} />
+            <ParentStack.Screen name="SignIn" component={SignInScreen} />
+          </ParentStack.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
   );
 }
